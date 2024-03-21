b57a1551d1919979f43a364ef6ec64243fbde08b
RRASH111 <ranishwaiki3@gmail.com>
2024-03-21 09:56:28 +0200

    add trpc, auth, dashbord

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+import React, { ReactNode } from 'react'
+
+const MaxWidthWrapper = ({
+    calssName,
+    children
+}:{
+    calssName: string,
+    children: ReactNode
+}) => {
+  return (
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', calssName)}>
+        {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
