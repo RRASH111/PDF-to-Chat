30cbe6708da7428a0e195adc3374bace58486d74
RRASH111 <ranishwaiki3@gmail.com>
2024-03-31 19:36:34 +0200

    Titles

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -20,7 +20,7 @@ import {
 } from 'lucide-react'
 import Link from 'next/link'
 
-const metadata = constructMetadata("home");
+const metadata = constructMetadata("pricing");
 
 const Page = async () => {
   const { getUser } = getKindeServerSession()
