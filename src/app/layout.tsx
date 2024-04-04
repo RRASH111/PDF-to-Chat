73fff26dc981dc3d7295bb6f7635052c62cd3aa3
RRASH111 <ranishwaiki3@gmail.com>
2024-04-05 00:48:17 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         inter.className
       )}>
         <Toaster/>
-        <Navbar />
+        <Navbar/>
         {children}
       </body>
       </CSPostHogProvider>
