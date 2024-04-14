c2011646acec231aafaa1b5b19f99e68e37c98a4
RRASH111 <ranishwaiki3@gmail.com>
2024-04-14 23:01:11 +0200

    Update route.ts

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -5,7 +5,9 @@ import { ourFileRouter } from "./core";
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
- 
-  // Apply an (optional) custom config:
-  // config: { ... },
+  config: {
+    uploadthingId: process.env.UPLOADTHING_APP_ID!,
+    uploadthingSecret: process.env.UPLOADTHING_SECRET!,
+    callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/uploadthing`,
+  },
 });
